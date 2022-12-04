c888e7bf1d7980d41c1d6176e05c11ad919bff66
Anastasia <plushvochka@gmail.com>
2022-12-04 18:34:15 +0300

    added task3 and 5

--- hw_2.py/task_4.py
+++ hw_2.py/task_4.py
@@ -1,4 +1,24 @@
-# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.(для продвинутых - с файлом, вариант минимум - ввести позиции в консоли)
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. 
+# Позиции хранятся в файле file.txt в одной строке одно число.(для продвинутых - с файлом, вариант минимум - ввести позиции в консоли)
 # -2 -1 0 1 2
 # Позиции: 0,1 -> 2
 
+with open('file_for_ex4.txt', 'w') as data:
+    data.write('-2\n')
+    data.write('-1\n')
+    data.write('0\n')
+    data.write('1\n')
+    data.write('2\n')
+
+with open('file_for_ex4.txt', 'r') as data:
+    firstIndex = int(f.readline())
+    secondIndex = int(f.readline())
+
+list_generation = [i for i in range(-n, n + 1)]
+mult = 1
+for p in pos:
+    mult *= list_generation[p]
+    
+print(pos)
+print(p)
+print(mult)
