feddddad481f30a90d9ba05a7b7c66c820fe95f6
Anastasia <plushvochka@gmail.com>
2022-12-04 21:06:59 +0300

    added task5 in hw2

--- hw_2.py/task_4.py
+++ hw_2.py/task_4.py
@@ -3,22 +3,23 @@
 # -2 -1 0 1 2
 # Позиции: 0,1 -> 2
 
-with open('file_for_ex4.txt', 'w') as data:
-    data.write('-2\n')
-    data.write('-1\n')
-    data.write('0\n')
-    data.write('1\n')
-    data.write('2\n')
+#with open('file_for_ex4.txt', 'w') as data:
+#   data.write('-2\n')
+#   data.write('-1\n')
+#   data.write('0\n')
+#   data.write('1\n')
+#   data.write('2\n')
 
 with open('file_for_ex4.txt', 'r') as data:
-    firstIndex = int(f.readline())
-    secondIndex = int(f.readline())
-
+    pos = data.read().split()
+pos = list(map(int, pos))
+    
+n = int(input())
 list_generation = [i for i in range(-n, n + 1)]
 mult = 1
 for p in pos:
     mult *= list_generation[p]
     
 print(pos)
-print(p)
+print(list_generation)
 print(mult)
